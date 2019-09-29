d28238e95ea89a05070052ef4d0fed4e3c39f724
przemwo <przemwo@o2.pl>
2019-09-29 16:29:37 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useCallback } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+const toggleState = {
+  initial: "closed",
+  closed: {
+    on: {
+      open: "opening",
+    }
+  },
+  opening: {
+    on: {
+      close: "closing",
+    },
+    effect: ({ msg }) => {
+      setTimeout(() => {
+        msg("open");
+      }, 500);
+    },
+    done: {
+      open: "open",
+    },
+  },
+  open: {
+    on: {
+      close: "closing",
+    }
+  },
+  closing: {
+    on: {
+      open: "opening",
+    },
+    effect: ({ msg }) => {
+      setTimeout(() => {
+        msg("close");
+      }, 500);
+    },
+    done: {
+      close: "closed",
+    },
+  },
+};
+
+const useMachine = (state) => {
+  const [current, setCurrent] = useState(state.initial);
+
+  const msg = useCallback((msg, canTransition = () => true) => {
+    if (!canTransition()) return;
+    if (state[current].on[msg]) {
+      setCurrent(state[current].on[msg])
+    } else if (state[current].done[msg]) {
+      setCurrent(state[current].done[msg]);
+    }
+  }, [current, state]);
+
+  useEffect(() => {
+    let canTransition = true;
+    state[current].effect && state[current].effect({ msg: (m) => msg(m, () => canTransition) });
+    return () => canTransition = false;
+  }, [current, state, msg]);
+
+  return { current, msg };
+};
+
+export const App = () => {
+  const machine = useMachine(toggleState);
+  console.log('current: ', machine.current)
+  return(
+    <div>
+      <h1>Current state: {machine.current}</h1>
+      <button
+        onClick={() => machine.msg("open")}
+        disabled={machine.current === "open" || machine.current === "opening"}
+      >
+        Open
+      </button>
+      <button
+        onClick={() => machine.msg("close")}
+        disabled={machine.current === "closed" || machine.current === "closing"}
+      >
+        Close
+      </button>
     </div>
   );
-}
-
-export default App;
+};
